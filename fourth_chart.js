b246bcf9631d76aa62d8b6e3e63f3a35999d387b
Node A <58964683+CollegeSeniorUser@users.noreply.github.com>
2024-04-25 20:46:33 -0700

    try fourth agin

--- fourth_chart.js
+++ fourth_chart.js
@@ -11,7 +11,7 @@
         .append("g")
         .attr("transform", `translate(50,30)`);
 
-        const margin = {top: 20, right: 20, bottom: 50, left: 50};
+        const margin = {top: 20, right: 20, bottom: 20, left: 20};
         const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
         let data = rawData.filter(d => d['State Population'].trim() !== "")
